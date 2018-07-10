ca4059332918fcc5d7ec32efa9f3e9455dca58e9
ZkEdison <214236543@qq.com>
2018-07-10 22:25:17 +0800

    lesson07

---
+++ lesson07/tests.js
@@ -0,0 +1,11 @@
+let should = chai.should()
+
+describe('simple test', () => {
+    it('should equal 0 when n === 0', () => {
+        window.fibonacci(0).should.equal(0)
+    })
+})
+
+/**
+ * 懵。。。
+ */
---
+++ lesson08/app.js
@@ -0,0 +1,40 @@
+let express = require('express')
+
+let fibonacci = (n) => {
+    if (typeof n !== 'number' || isNaN(n)) {
+        throw new Error('n should be a Number')
+    }
+    if (n < 0) {
+        throw new Error('n should >= 0')
+    }
+    if (n > 10) {
+        throw new Error('n should  <= 10')
+    }
+    if (n === 0) {
+        return 0
+    }
+    if (n === 1) {
+        return 1
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2)
+}
+
+let app = express()
+
+app.get('/fib', (req, res) => {
+    let n = Number(req.query.n)
+    try {
+        res.send(String(fibonacci(n)))
+    } catch (e) {
+        res
+            .status(500)
+            .send(e.message)
+    }
+})
+
+app.listen(3000, () => {
+    console.log('app is listening at port 3000')
+})
+
+
+module.exports = app
